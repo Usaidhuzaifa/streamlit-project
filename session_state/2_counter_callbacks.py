4de3aafd6d522e1c63a16285acceab99515148a7
Usaid Huzaifa <114611598+Usaidhuzaifa@users.noreply.github.com>
2023-12-23 00:09:26 -0800

    Add files via upload

---
+++ session_state/2_counter_callbacks.py
@@ -0,0 +1,19 @@
+import streamlit as st
+
+st.title('Counter Example using Callbacks')
+if 'count' not in st.session_state:
+    st.session_state.count = 0
+
+def increment_counter():
+    st.session_state.count += 1
+
+st.button('Increment', on_click=increment_counter)
+
+st.write('Count = ', st.session_state.count)
+
+def decrement_counter():
+    st.session_state.count -= 1
+
+st.button('decrement', on_click=decrement_counter)
+
+st.write('Count = ', st.session_state.count)
